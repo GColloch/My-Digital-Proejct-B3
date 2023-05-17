cbd1af25bc4138d81ad03953f2260851704d86da
Guillaume <guillaumecolloch56@gmail.com>
2023-05-17 18:35:56 +0200

    fin bloc services

--- frontend/src/components/NosServices.jsx
+++ frontend/src/components/NosServices.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../styles/NosServices.scss'
+
 const NosServices = () => {
   return (
-    <>
-      <div id='bloc'>
-        <h1><span>NOS SERV</span>ICES</h1>
-        <div class='services'>
-          <img src='logo1.png' alt='Logo 1' />
+    <div id='bloc'>
+      <div className='title'>
+        <h1>
+          <span>NOS SERV</span>ICES
+        </h1>
+      </div>
+      <div className='card-container'>
+        <div className='services'>
+          <img className='démarches' src='/assets/img/Accueil/démarches.png' alt='Logo 1' />
           <h2>LES DÉMARCHES</h2>
           <p>
             Immobilières, successions, <br />
@@ -17,8 +22,8 @@ const NosServices = () => {
           </p>
           <button>Découvrez</button>
         </div>
-        <div class='services'>
-          <img src='logo2.png' alt='Logo 2' />
+        <div className='services'>
+          <img className='cérémonies' src='/assets/img/Accueil/cérémonies.png' alt='Logo 2' />
           <h2>LES CÉRÉMONIES</h2>
           <p>
             En fonction de votre religion (athée, <br />
@@ -27,8 +32,8 @@ const NosServices = () => {
           </p>
           <button>Découvrez</button>
         </div>
-        <div class='services'>
-          <img src='logo3.png' alt='Logo 3' />
+        <div className='services'>
+          <img className='tarificateur' src='/assets/img/Accueil/tarificateur.png' alt='Logo 3' />
           <h2>LE TARIFICATEUR</h2>
           <p>
             Choisissez à ce que vous souhaitez pour <br />
@@ -40,7 +45,7 @@ const NosServices = () => {
           <button>Découvrez</button>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
