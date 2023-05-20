5784c4e0f64cd335aaf616209b0bac145d212313
Guillaume <guillaumecolloch56@gmail.com>
2023-05-20 12:45:19 +0200

    debut responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,22 +2,34 @@
 
 import React from 'react'
 import '../styles/_footer.scss'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { FaFacebookSquare } from 'react-icons/fa'
+import { IoLogoLinkedin } from 'react-icons/io'
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div className='logo-containerfooter'>
-        <img className='logofooter' src='path/to/logo.png' alt='Logo' />
+    <>
+
+      <div className='containerfooter'>
+        <p>RETROUVEZ TOUTES LES RÉPONSES À VOS QUESTIONS</p>
+        <button>Notre FAQ</button>
       </div>
-      <div className='text-containerfooter'>
-        <p className='centered-textfooter'>@ 2023 - NEFTI - Tous droits réservés - politique de confidentialité - Cookies - Mentions Légales</p>
-      </div>
-      <div className='picto-containerfooter'>
-        <img className='picto' src='path/to/picto1.png' alt='Pictogramme 1' />
-        <img className='picto' src='path/to/picto2.png' alt='Pictogramme 2' />
-        <img className='picto' src='path/to/picto3.png' alt='Pictogramme 3' />
-      </div>
-    </footer>
+      <footer className='footer'>
+        <div className='text-containerfooter'>
+          <p className='centered-textfooter'>
+            @ 2023 - NEFTI - Tous droits réservés - politique de confidentialité - Cookies - Mentions Légales
+          </p>
+        </div>
+        <div className='logo-containerfooter'>
+          <img className='logofooter' src='assets/img/Accueil/logofooter.png' alt='Logo' />
+          <div className='picto-containerfooter'>
+            <FaFacebookSquare className='pictofooter' />
+            <IoLogoLinkedin className='pictofooter' />
+            <AiOutlineInstagram className='pictofooter' />
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
